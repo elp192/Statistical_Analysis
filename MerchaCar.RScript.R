0b55f486aa4142d1569aa4559ff6cfbd7eff8186
Elnaz <e.pouranbarani@gmail.com>
2021-10-05 10:31:05 -0400

    Determine T_Tests on suspensions coils

--- MerchaCar.RScript.R
+++ MerchaCar.RScript.R
@@ -1,21 +1,35 @@
-# Import package
+# Import package.
 library(dplyr)
 
-# Read MechaCar_mpg data
+# Read MechaCar_mpg data.
 table1<-read.csv(file='data/MechaCar_mpg.csv')
 
-# Perform multiple linear regression
+# Perform multiple linear regression.
 lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=table1)
 
-# Determine summary statistics 
+# Determine summary statistics.
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=table1))
 
-# Read Suspension_Coil data 
+# Read Suspension_Coil data.
 table2 <- read.csv(file='data/Suspension_Coil.csv')
 
-# Create total summary (mean, median, variance, and std)
+# Create total summary (mean, median, variance, and std).
 total_summary<-table2 %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 
-# Create lot summary 
+# Create lot summary.
 lot_summary<- table2 %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .group='keep')
+
+# Determine if the PSI across manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(table2$PSI,mu=1500)
+
+# Determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(subset(table2,Manufacturing_Lot=Lot1)$PSI,mu=1500)
+
+t.test(subset(table2,Manufacturing_Lot=Lot2)$PSI,mu=1500)
+
+t.test(subset(table2,Manufacturing_Lot=Lot3)$PSI,mu=1500)
+
+
+
+
 
